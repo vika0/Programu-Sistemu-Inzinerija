1106ecd6fd26d6a0d711f237b9b3fcbecdc99e5a
Viktorija Razaite <viktorija.razaite@gmail.com>
2017-03-30 18:13:51 +0300

    new controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,6 +13,8 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        echo "puslapis";
+        echo "<a href='http://127.0.0.1:8001/about'>Apie mus</a>";
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
