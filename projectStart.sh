70aa05ba9c4a3f0e75a31c2b0b344dbf5045f53e
Viktorija Razaite <viktorija.razaite@gmail.com>
2017-04-03 10:14:17 +0300

    script to start server and project

---
+++ projectStart.sh
@@ -0,0 +1,7 @@
+#start server
+cd /var/www/Programu-Sistemu-Inzinerija
+sudo php bin/console server:start 127.0.0.1:8001
+
+#start phpstorm
+cd /home/vika/Downloads/PhpStorm-163.13906.21/bin
+sudo ./phpstorm.sh
