f30509b823a5fb3b826ab5dd3ec7b240d5c00f4d
Viktorija Razaite <viktorija.razaite@gmail.com>
2017-05-16 20:55:53 +0300

    taking client data from database

---
+++ src/AppBundle/Controller/ClientController.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 17.3.29
+ * Time: 20.59
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class ClientController extends Controller
+{
+    /**
+     * @Route("/client")
+     */
+    public function indexAction(Request $request)
+    {
+
+        return $this->render('default/client/client.html.twig', [
+//            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/client/list")
+     */
+    public function listAction()
+    {
+        return $this->render('default/client/client.html.twig', array(
+            'clients' => $this->getDoctrine()->getRepository('alkani\PSIBundle\Entity\Client')->findAll()
+        ));
+    }
+
+}
